22cbbc1923b23162be7421ff38d86ab570063b5d
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-06-06 12:54:24 +0600

    first commit

---
+++ Watchlist/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Movie
+from django.http import JsonResponse
+
+# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies' : list(movies)
+    }
+    return JsonResponse(context)
+
+    
