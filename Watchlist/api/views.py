e2aef71801deb256bc8e594f160e1d583dec43c9
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-06-07 18:28:48 +0600

    Serializin data usning serializer class not modelserializer

--- Watchlist/api/views.py
+++ Watchlist/api/views.py
@@ -3,16 +3,27 @@ from rest_framework.response import Response
 from Watchlist.models import Movie
 from Watchlist.api.serializers import MovieSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view()
 def movie_details(request,pk):
     movie = Movie.objects.get( pk=pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
+
