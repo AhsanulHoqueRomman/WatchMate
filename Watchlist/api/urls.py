20f369f313c4d9a73c1c86712e8f60e17aa597c4
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-06-10 19:26:15 +0600

    Adding new models and new serializer class

--- Watchlist/api/urls.py
+++ Watchlist/api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('list/', views.WatchListAV.as_view(), name='WatchListAV'),
     path('list/<int:pk>/', views.WatchDetailsAV.as_view(), name='WatchDetailsAV'),
+    
+    path('platform/', views.StreamPlatformAV.as_view(), name='platform'),
+    path('platform/<int:pk>', views.StreamPlatformDetailAV.as_view() , name='platform'),
 ]
