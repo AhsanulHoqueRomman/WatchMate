1e383bfff03c4bac7acf620763c3668410a870b8
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-06-24 23:56:15 +0600

    Implementing RegistrationSerializers and custom create function

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from rest_framework.authtoken.views import obtain_auth_token
+
+from user.api import views
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/',views.registration_view, name='register'),
 ]
 
