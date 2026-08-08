6928bd61d407006aef79988904d514d798882500
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-08-09 02:33:38 +0600

    Added login and logout test cases

--- user/tests.py
+++ user/tests.py
@@ -101,3 +101,78 @@ class RegisterTestCase(APITestCase):
         self.client.post(self.url,self.valid_data,format="json")
         user = User.objects.get(username="romman")
         self.assertTrue(user.check_password("StrongPassword123"))
+        
+        
+class LoginTestCase(APITestCase):
+    
+    def setUp(self):
+        self.url = reverse("login")
+        self.username = 'romman'
+        self.password= 'StrongPassword123'
+        
+        self.user = User.objects.create_user(
+            username= self.username,
+            password= self.password
+        )
+        
+    def test_login(self):
+        data = {
+            'username': self.username,
+            'password' : self.password
+        }
+        
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("token", response.data)
+        
+    
+    def test_wrong_password(self):
+        data = {
+            'username': self.username,
+            'password' : 'jduhdullem'
+        }       
+        response = self.client.post(self.url, data, format= 'json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_wrong_username(self):
+        data = {
+            'username': 'user',
+            'password' : self.password
+        }        
+        response = self.client.post(self.url, data, format= 'json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    def test_non_existent_user(self):
+        data = {
+            'username': 'user',
+            'password' : 234
+        }        
+        response = self.client.post(self.url, data, format= 'json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+
+
+class LogOutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="romman",
+            password="NewPassword@123"
+        )
+
+        self.token = Token.objects.create(user=self.user)
+        # self.token = Token.objects.get(user=self.user)
+        self.url = reverse("logout")
+        
+    
+    def test_successful_logout(self):
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+ self.token.key)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        
+    # def test_logout_without_authentication(self):
+        
+    #     response = self.client.post(self.url)
+    #     self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+    
